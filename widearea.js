6005d71f121a733d04d6117792b9bc25d19b82e9
Afshin Mehrabani <afshin.meh@gmail.com>
2013-05-21 17:36:45 +0430

    Add setOption(s) methods

--- widearea.js
+++ widearea.js
@@ -197,6 +197,20 @@
     }
   }
 
+  /**
+   * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+   *
+   * @param obj1
+   * @param obj2
+   * @returns obj3 a new object based on obj1 and obj2
+   */
+  function _mergeOptions(obj1, obj2) {
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+  }
+
   var wideArea = function (selector) {
     if (typeof (selector) === 'string') {
       //select the target element with query selector
@@ -224,7 +238,15 @@
   wideArea.fn = WideArea.prototype = {
     clone: function () {
       return new WideArea(this);
-    }
+    },
+    setOption: function(option, value) {
+      this._options[option] = value;
+      return this;
+    },
+    setOptions: function(options) {
+      this._options = _mergeOptions(this._options, options);
+      return this;
+    },
   };
 
   exports.wideArea = wideArea;
